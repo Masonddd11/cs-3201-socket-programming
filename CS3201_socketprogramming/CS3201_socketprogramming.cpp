0ef6f74fc7b485c596b88d4014b37566307e17a8
結露 <68598198+XketsuroX@users.noreply.github.com>
2024-10-19 02:19:14 +0800

    Project init
    Basic init of project.
    Based on CMake.
    modified gitignore to fit in VS2022 env

---
+++ CS3201_socketprogramming/CS3201_socketprogramming.cpp
@@ -0,0 +1,17 @@
+﻿// CS3201_socketprogramming.cpp: 定義應用程式的進入點。
+//
+
+#include "CS3201_socketprogramming.h"
+
+
+
+using namespace std;
+int main()
+{
+
+	sockpp::initialize();
+
+	sockpp::tcp_connector conn;
+
+	return 0;
+}
---
+++ CS3201_socketprogramming/CS3201_socketprogramming.h
@@ -0,0 +1,10 @@
+﻿// CS3201_socketprogramming.h: 標準系統 include 檔案的 include 檔案，
+// 或專案專屬的 Include 檔案。
+
+#pragma once
+
+#include <iostream>
+#include <string>
+// TODO: 在此參考您的程式所需的其他標頭。
+
+#include <sockpp/tcp_connector.h>
