3ec258eb2283a882700d6de2e4faca538c86ff1f
結露 <pt111037@gmail.com>
2024-10-19 18:13:49 +0800

    library changed

---
+++ socket/socket.cpp
@@ -0,0 +1,173 @@
+﻿// socket.cpp: 定義應用程式的進入點。
+//
+
+#include "socket.h"
+
+using namespace std;
+
+class nmessage {
+public:
+	virtual string formal() { return msg; }
+	nmessage(string i) :msg(i) {}
+	nmessage() {}
+private:
+	string msg;
+
+};
+
+class Delete : public nmessage {
+private:
+	vector <string>content;
+public:
+	Delete() {
+		string sentance;
+		getline(cin, sentance);
+
+		while (sentance[0] != '#')
+		{
+			content.push_back(sentance);
+			getline(cin, sentance);
+		}
+	}
+	string formal() {
+		string ret;
+		for (auto s : content) {
+			ret.append(s);
+			ret.append("\n");
+		}
+		return ret;
+	}
+};
+
+class Quit : public nmessage {
+public:
+	Quit() {};
+	string formal() {
+		return "QUIT";
+	}
+};
+
+class Get : public nmessage {
+public:
+	Get() {};
+	string formal() {
+		return "GET";
+	}
+
+};
+
+class Post : public nmessage {
+private:
+	vector <string>content;
+public:
+	Post() {
+		string sentance;
+		getline(cin, sentance);
+
+		while (sentance[0] != '#')
+		{
+			content.push_back(sentance);
+			getline(cin, sentance);
+		}
+	}
+	string formal() {
+		string ret;
+		for (auto s : content) {
+			ret.append(s);
+			ret.append("\n");
+		}
+		return ret;
+	}
+
+};
+
+class socket
+{
+public:
+	socket(string ip, auto port);
+	~socket();
+	void start() { tcp_client.open(); }
+	void addcommand(string m);
+	void sendAndreceive();
+	void block();
+private:
+	nmessage* packet;
+	MinimalSocket::Port server_port;
+	MinimalSocket::tcp::TcpClient<true> tcp_client;
+	string address;
+
+};
+
+socket::socket(string ip, auto port):address(ip),server_port(port),tcp_client(MinimalSocket::Address{ ip,port }),packet(nullptr){}
+
+socket::~socket() { tcp_client.shutDown(); }
+
+void socket::addcommand(const string m)
+{
+	if (m == "POST")
+		packet = new Post;
+	else if (m == "GET")
+		packet = new Get();
+	else if (m == "DELETE")
+		packet = new Delete();
+	else if (m == "QUIT")
+		packet = new Quit();
+	else packet = new nmessage(m);
+
+}
+
+void socket::sendAndreceive()
+{
+	string rcvbuf;
+	//check method, if string is needed, send METHOD to server to initiate it first
+	if (typeid(Post) == typeid(*packet))
+		tcp_client.send("POST");
+	else if (typeid(Delete) == typeid(*packet))
+		tcp_client.send("DELETE");
+
+	// Tie string from multiple line into one
+	tcp_client.send(packet->formal());
+
+	// As sending the packets so rapidly will lead to packet discard from server, time is need to for server to clear buffer
+	this_thread::sleep_for(chrono::milliseconds(300));
+
+	if (typeid(Post) == typeid(*packet) || typeid(Delete) == typeid(*packet))
+	{
+		tcp_client.send("#");
+		this_thread::sleep_for(chrono::milliseconds(300));
+	}
+	
+
+	rcvbuf = tcp_client.receive(4096);
+	
+	if(typeid(Get) ==  typeid(*packet))
+		while (rcvbuf.back() != '#') {
+			cout << rcvbuf << endl;
+			this_thread::sleep_for(chrono::milliseconds(300));
+			rcvbuf = tcp_client.receive(4096);
+		}
+
+
+	cout << rcvbuf << endl;
+	delete packet;
+	packet = nullptr;
+}
+
+
+
+
+
+
+
+int main()
+{
+	
+	socket c_socket("127.0.0.1", MinimalSocket::Port(16111));
+	c_socket.start();
+	string command;
+	while (cin >> command) {
+		c_socket.addcommand(command);
+		c_socket.sendAndreceive();
+	}
+	return 0;
+}
---
+++ socket/socket.h
@@ -0,0 +1,14 @@
+﻿// socket.h: 標準系統 include 檔案的 include 檔案，
+// 或專案專屬的 Include 檔案。
+
+#pragma once
+
+#include <iostream>
+
+// TODO: 在此參考您的程式所需的其他標頭。
+
+#include <MinimalSocket/tcp/TcpServer.h>
+#include <MinimalSocket/tcp/TcpClient.h>
+#include <string>
+#include <thread>
+#include <chrono>
